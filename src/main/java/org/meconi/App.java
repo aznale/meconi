304ea31cf95a65d97b63d497d0af6d610a0a2e88
aznale <sergiaznaralemany@gmail.com>
2023-01-12 12:45:33 +0100

    meconi canvi text 2

--- src/main/java/org/meconi/App.java
+++ src/main/java/org/meconi/App.java
@@ -1,8 +1,9 @@
 package org.meconi;
 
-public class App
-{
-    public static void main( String[] args ) {
-        System.out.println( "Hello Meconi!" );
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Meconi!");
+        System.out.println("Bye bye Meconi");
     }
 }
